cd23f3493cb85fe1ff8292fdc5c910a443f412f9
Homero <homiecazares16@gmail.com>
2024-02-13 19:12:18 -0600

    added email function to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from "@emailjs/browser";
 
+
+
+const serviceId = import.meta.env.VITE_SERVICE_ID;
+const templateId = import.meta.env.VITE_TEMPLATE_ID;
+const publicKey = import.meta.env.VITE_PUBLIC_KEY
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `${import.meta.env.VITE_SERVICE_ID}`,
+        `${import.meta.env.VITE_TEMPLATE_ID}`,
+        form.current,
+        {
+          publicKey: `${import.meta.env.VITE_PUBLIC_KEY}`,
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+  
+  
   return (
     <section id="contact" className="bg-gray-200 py-12">
       <div className="container mx-auto">
@@ -13,24 +44,28 @@ function Contact() {
             Have a question or want to get in touch? Feel free to reach out to
             me via the form below:
           </p>
-          <form className="grid grid-cols-1 gap-4">
+          <form ref={form} onSubmit={sendEmail} className="grid grid-cols-1 gap-4">
             <input
               type="text"
+              name='user_name'
               placeholder="Your Name"
               className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
             <input
               type="email"
+              name='user_email'
               placeholder="Your Email"
               className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
-            <textarea
+            <textarea 
+              name='message'
               placeholder="Your Message"
               rows="4"
               className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             ></textarea>
             <button
               type="submit"
+              value="Send"
               className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 transition-colors duration-300"
             >
               Send Message
