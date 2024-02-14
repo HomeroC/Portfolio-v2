b9a08c35c63051038def6c73727302af3deb747d
Homero <homiecazares16@gmail.com>
2024-02-13 20:22:18 -0600

    finished email funtionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,26 @@
-import React, {useRef} from 'react'
+import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-
-
 
 const serviceId = import.meta.env.VITE_SERVICE_ID;
 const templateId = import.meta.env.VITE_TEMPLATE_ID;
-const publicKey = import.meta.env.VITE_PUBLIC_KEY
+const publicKey = import.meta.env.VITE_PUBLIC_KEY;
 
 function Contact() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    console.log(serviceId)
+    console.log(templateId)
+    console.log(publicKey)
+    
     emailjs
       .sendForm(
-        `${import.meta.env.VITE_SERVICE_ID}`,
-        `${import.meta.env.VITE_TEMPLATE_ID}`,
+        `${serviceId}`,
+        `${templateId}`,
         form.current,
         {
-          publicKey: `${import.meta.env.VITE_PUBLIC_KEY}`,
+          publicKey: `${publicKey}`,
         }
       )
       .then(
@@ -31,8 +32,7 @@ function Contact() {
         }
       );
   };
-  
-  
+
   return (
     <section id="contact" className="bg-gray-200 py-12">
       <div className="container mx-auto">
@@ -44,21 +44,25 @@ function Contact() {
             Have a question or want to get in touch? Feel free to reach out to
             me via the form below:
           </p>
-          <form ref={form} onSubmit={sendEmail} className="grid grid-cols-1 gap-4">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="grid grid-cols-1 gap-4"
+          >
             <input
               type="text"
-              name='user_name'
+              name="user_name"
               placeholder="Your Name"
               className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
             <input
               type="email"
-              name='user_email'
+              name="user_email"
               placeholder="Your Email"
               className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
-            <textarea 
-              name='message'
+            <textarea
+              name="message"
               placeholder="Your Message"
               rows="4"
               className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
@@ -77,4 +81,4 @@ function Contact() {
   );
 }
 
-export default Contact
+export default Contact;
